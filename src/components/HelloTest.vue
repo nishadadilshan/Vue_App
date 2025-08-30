f5eb1bd6829320e22f6ead7d246320bd46b0978a
Dilshan Weerathunga <dilshan.weerathunga@synapsys.lk>
2025-08-30 09:59:12 +0200

    testing components

---
+++ src/components/HelloTest.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="hello-test">
+        <p>Hello Test</p>
+        <h1>{{ msg }}</h1>    
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'HelloTest',
+        props: {
+            msg: String
+        }
+    }
+</script>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <HelloTest :msg="msg" />
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
@@ -31,8 +32,13 @@
 </template>
 
 <script>
+import HelloTest from './HelloTest.vue'
+
 export default {
   name: 'HelloWorld',
+  components: {
+    HelloTest
+  },
   props: {
     msg: String
   }
